35910e1fa6883d110e9eea09da442de22f522ab9
Kabir Dhillon <kabirdhillon99@gmail.com>
2024-08-06 14:36:17 -0700

    Initial commit

---
+++ Meals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meals
+//
+//  Created by Kabir Dhillon on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meals/MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsApp.swift
+//  Meals
+//
+//  Created by Kabir Dhillon on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
